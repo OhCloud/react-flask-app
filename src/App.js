a62106caa46641439650702269c277030b111e76
Cloudy Lopez <CloudyLopez@icloud.com>
2021-07-26 11:25:07 -0700

    adding untracked files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [initalData, setInitialData] = useState([{}])
+
+  useEffect(()=> {
+    fetch('/api').then(
+      response => response.json()
+    ).then(data => setInitialData(data))
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{initalData.title}</h1>
     </div>
   );
 }
